832858b6e94e9287faeeebb53b6fefd3666db32f
Greg <grzegorz.surma@icloud.com>
2018-10-06 22:26:41 -0700

    base & memory
    Former-commit-id: 705d47ac5cd96c079372a6e5101921324e885b8b

--- text_predictor.py
+++ text_predictor.py
@@ -27,12 +27,27 @@ LOGGING_FREQUENCY = 1e3
 TEXT_SAMPLING_FREQUENCY = 1e3
 GRADIENT_LIMIT = 5
 
-# Model
-input_to_hidden = np.random.randn(HIDDEN_LAYER_SIZE, vocabulary_size) * 0.01
-hidden_to_hidden = np.random.randn(HIDDEN_LAYER_SIZE, HIDDEN_LAYER_SIZE) * 0.01
-hidden_to_output = np.random.randn(vocabulary_size, HIDDEN_LAYER_SIZE) * 0.01
-hidden_bias = np.zeros((HIDDEN_LAYER_SIZE, 1))
-output_bias = np.zeros((vocabulary_size, 1))
+# # Model
+# input_to_hidden = np.random.randn(HIDDEN_LAYER_SIZE, vocabulary_size) * 0.01
+# hidden_to_hidden = np.random.randn(HIDDEN_LAYER_SIZE, HIDDEN_LAYER_SIZE) * 0.01
+# hidden_to_output = np.random.randn(vocabulary_size, HIDDEN_LAYER_SIZE) * 0.01
+# hidden_bias = np.zeros((HIDDEN_LAYER_SIZE, 1))
+# output_bias = np.zeros((vocabulary_size, 1))
+
+class RNNModel:
+
+    def __init__(self, random_init=False):
+        self.input_to_hidden = (np.random.randn(HIDDEN_LAYER_SIZE, vocabulary_size) * 0.01) if random_init else np.zeros((HIDDEN_LAYER_SIZE, vocabulary_size))
+        self.hidden_to_hidden = (np.random.randn(HIDDEN_LAYER_SIZE, HIDDEN_LAYER_SIZE) * 0.01) if random_init else np.zeros((HIDDEN_LAYER_SIZE, HIDDEN_LAYER_SIZE))
+        self.hidden_to_output = (np.random.randn(vocabulary_size, HIDDEN_LAYER_SIZE) * 0.01) if random_init else np.zeros((vocabulary_size, HIDDEN_LAYER_SIZE))
+        self.hidden_bias = np.zeros((HIDDEN_LAYER_SIZE, 1))
+        self.output_bias = np.zeros((vocabulary_size, 1))
+
+    def __iter__(self):
+        return iter([self.input_to_hidden, self.hidden_to_hidden, self.hidden_to_output, self.hidden_bias, self.output_bias])
+
+
+base = RNNModel(random_init=True)
 
 
 def update(inputs, targets, hidden_previous):
@@ -43,25 +58,25 @@ def update(inputs, targets, hidden_previous):
     for t in xrange(len(inputs)):
         input_state[t] = np.zeros((vocabulary_size, 1))  # encode in 1-of-k representation
         input_state[t][inputs[t]] = 1
-        hidden_state[t] = np.tanh(np.dot(input_to_hidden, input_state[t]) + np.dot(hidden_to_hidden, hidden_state[t - 1]) + hidden_bias)  # hidden state
-        output_state[t] = np.dot(hidden_to_output, hidden_state[t]) + output_bias  # unnormalized log probabilities for next chars
+        hidden_state[t] = np.tanh(np.dot(base.input_to_hidden, input_state[t]) + np.dot(base.hidden_to_hidden, hidden_state[t - 1]) + base.hidden_bias)  # hidden state
+        output_state[t] = np.dot(base.hidden_to_output, hidden_state[t]) + base.output_bias  # unnormalized log probabilities for next chars
         probabilites_state[t] = np.exp(output_state[t]) / np.sum(np.exp(output_state[t]))  # probabilities for next chars
         loss += -np.log(probabilites_state[t][targets[t], 0])  # softmax (cross-entropy loss)
     # backward pass: compute gradients going backwards
-    gradient_input_to_hidden, gradient_hidden_to_hidden, gradient_hidden_to_output = np.zeros_like(input_to_hidden), np.zeros_like(hidden_to_hidden), np.zeros_like(hidden_to_output)
-    gradient_hidden_bias, gradient_output_bias = np.zeros_like(hidden_bias), np.zeros_like(output_bias)
+    gradient_input_to_hidden, gradient_hidden_to_hidden, gradient_hidden_to_output = np.zeros_like(base.input_to_hidden), np.zeros_like(base.hidden_to_hidden), np.zeros_like(base.hidden_to_output)
+    gradient_hidden_bias, gradient_output_bias = np.zeros_like(base.hidden_bias), np.zeros_like(base.output_bias)
     gradient_hidden_next = np.zeros_like(hidden_state[0])
     for t in reversed(xrange(len(inputs))):
         gradient_output = np.copy(probabilites_state[t])
         gradient_output[targets[t]] -= 1  # backprop into y. see http://cs231n.github.io/neural-networks-case-study/#grad if confused here
         gradient_hidden_to_output += np.dot(gradient_output, hidden_state[t].T)
         gradient_output_bias += gradient_output
-        gradient_hidden = np.dot(hidden_to_output.T, gradient_output) + gradient_hidden_next  # backprop into h
+        gradient_hidden = np.dot(base.hidden_to_output.T, gradient_output) + gradient_hidden_next  # backprop into h
         gradient_hidden_raw = (1 - hidden_state[t] * hidden_state[t]) * gradient_hidden  # backprop through tanh nonlinearity
         gradient_hidden_bias += gradient_hidden_raw
         gradient_input_to_hidden += np.dot(gradient_hidden_raw, input_state[t].T)
         gradient_hidden_to_hidden += np.dot(gradient_hidden_raw, hidden_state[t - 1].T)
-        gradient_hidden_next = np.dot(hidden_to_hidden.T, gradient_hidden_raw)
+        gradient_hidden_next = np.dot(base.hidden_to_hidden.T, gradient_hidden_raw)
     for gradient_param in [gradient_input_to_hidden, gradient_hidden_to_hidden, gradient_hidden_to_output, gradient_hidden_bias, gradient_output_bias]:
         np.clip(gradient_param, -GRADIENT_LIMIT, GRADIENT_LIMIT, out=gradient_param)  # clip to mitigate exploding gradients
     return loss, gradient_input_to_hidden, gradient_hidden_to_hidden, gradient_hidden_to_output, gradient_hidden_bias, gradient_output_bias, hidden_state[len(inputs) - 1]
@@ -81,8 +96,8 @@ def sample_text(previous_hidden, seed_index, n):
     input[seed_index] = 1
     indices = []
     for t in xrange(n):
-        previous_hidden = np.tanh(np.dot(input_to_hidden, input) + np.dot(hidden_to_hidden, previous_hidden) + hidden_bias)
-        output = np.dot(hidden_to_output, previous_hidden) + output_bias
+        previous_hidden = np.tanh(np.dot(base.input_to_hidden, input) + np.dot(base.hidden_to_hidden, previous_hidden) + base.hidden_bias)
+        output = np.dot(base.hidden_to_output, previous_hidden) + base.output_bias
         probability = np.exp(output) / np.sum(np.exp(output))
         index = np.random.choice(range(vocabulary_size), p=probability.ravel())
         input = np.zeros((vocabulary_size, 1))
@@ -95,11 +110,13 @@ def rnn():
     iteration = 0
     data_pointer = 0
 
-    memory_input_to_hidden = np.zeros_like(input_to_hidden)
-    memory_hidden_to_hidden = np.zeros_like(hidden_to_hidden)
-    memory_hidden_to_output = np.zeros_like(hidden_to_output)
-    memory_bias_hidden = np.zeros_like(hidden_bias)
-    memory_bias_output = np.zeros_like(output_bias)
+    memory_input_to_hidden = np.zeros_like(base.input_to_hidden)
+    memory_hidden_to_hidden = np.zeros_like(base.hidden_to_hidden)
+    memory_hidden_to_output = np.zeros_like(base.hidden_to_output)
+    memory_bias_hidden = np.zeros_like(base.hidden_bias)
+    memory_bias_output = np.zeros_like(base.output_bias)
+
+    memory = RNNModel()
 
     smooth_loss = None
     smooth_losses = []
@@ -133,11 +150,11 @@ def rnn():
             plot_smooth_loss(smooth_losses)
 
         # perform parameter update with Adagrad
-        for param, gradient_param, memory in zip([input_to_hidden, hidden_to_hidden, hidden_to_output, hidden_bias, output_bias],
-                                                 [gradient_input_to_hidden, gradient_hidden_to_hidden, gradient_hidden_to_output, gradient_hidden_bias, gradient_output_bias],
-                                                 [memory_input_to_hidden, memory_hidden_to_hidden, memory_hidden_to_output, memory_bias_hidden, memory_bias_output]):
-            memory += gradient_param * gradient_param
-            param += -LEARNING_RATE * gradient_param / np.sqrt(memory + ADAGRAD_UPDATE_RATE)
+        for base_param, gradient_param, memory_param in zip(base,
+                                                            [gradient_input_to_hidden, gradient_hidden_to_hidden, gradient_hidden_to_output, gradient_hidden_bias, gradient_output_bias],
+                                                            memory):
+            memory_param += gradient_param * gradient_param
+            base_param += -LEARNING_RATE * gradient_param / np.sqrt(memory_param + ADAGRAD_UPDATE_RATE)
 
         data_pointer += SEQUENCE_LENGTH
         iteration += 1
